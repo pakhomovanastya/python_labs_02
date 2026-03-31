23228aa11ea2209a74f44bc197b8ef5609c98a28
pakhomovanastya <pakhomova.nastya.lol@gmail.com>
2026-03-31 17:28:06 +0300

    лаба 1

--- src/lab01/demo.py
+++ src/lab01/demo.py
@@ -11,20 +11,20 @@ print("\n> Три экземпляра класса Student с разными п
 print(student1) #вызывает __str__
 print(student2)
 print(student3)
-print("-вызываем __repr__-")
+print("--вызываем __repr__--")
 print(repr(student3)) #вызывает __repr__
 
 
 print("\n> Сравнение объектов, где вызываем __eq__")
 if student1 == student2: #вызывает __eq__
-    print(f"-{student1} и {student2} один и тот же студент")
+    print(f"--{student1.surname} и {student2.surname} один и тот же студент")
 else:
-    print(f"-{student1} и {student2} разные студенты")
+    print(f"-{student1.surname} и {student2.surname} разные студенты")
 
 if student1 == student4: #вызывает __eq__
-    print(f"-{student1} и {student2} один и тот же студент")
+    print(f"-{student1.surname} и {student4.surname} один и тот же студент")
 else:
-    print(f"-{student1} и {student2} разные студенты")
+    print(f"-{student1.surname} и {student4.surname} разные студенты")
 
 
 print('\n> Обработка ошибок: фамилия передана как число')
@@ -55,7 +55,7 @@ for i in range(4):
             new_gpa = randint(3, 5)
             print(f"-Устанавливаем новый GPA: {new_gpa}")
             student2.gpa = new_gpa
-            print(f"-Студент переведён на {student2.curse} курс, новый балл: {student2.gpa}")
+            print(f"-Студент переведён на {student2.curse} курс, новый балл: {student2.gpa}\n")
         else:
             print(f"-Студент не может быть переведён (текущий GPA: {student2.gpa})")
     except ValueError as e:
@@ -66,15 +66,15 @@ print("\n> Сценарий 2: Попытка перевода студента 
 print(f"-Студент: {student1}")
 if student1.chek_to_next_curse():
     student1.to_next_course()
-    print(f"-Студент {student1.surname} переведён на курс {student1.curse}")
+    print(f"-Студент {student1.surname} переведён на курс {student1.curse}\n")
 else:
     print(f"-Студент {student1.surname} не прошёл проверку по среднему баллу {student1.gpa}")
 
 
 print("\n> Сценарий 3: Демонстрация логического состояния (неактивный студент)")
-print(f"-Исходное состояние: {student3}")
+print(f"-Исходное состояние: {student3.surname} активен")
 student3.not_active()  # Изменение состояния
-print(f"-После вызова not_active(): {student3}")
+print(f"-После вызова not_active(): {student3.surname} неактивен(а)")
 try:
     if student3.chek_to_next_curse():
         student3.to_next_course()
@@ -96,26 +96,26 @@ else:
 
 
 print("\n> Сценарий 5: Демонстрация работы метода to_collect_money()")
-print(f"-Стипендия студента {student1.surname}: {student1.to_collect_money()} руб.")
-print(f"-Стипендия студента {student2.surname}: {student2.to_collect_money()} руб.")
-print(f"-Стипендия студента {student3.surname}: {student3.to_collect_money()} руб.")
+print(f"-Стипендия студента {student1.surname}: {student1.to_collect_money()} руб., так как его GPA {student1.gpa}")
+print(f"-Стипендия студента {student2.surname}: {student2.to_collect_money()} руб., так как его GPA {student2.gpa}")
+print(f"-Стипендия студента {student3.surname}: {student3.to_collect_money()} руб., так как его GPA {student3.gpa}")
 
 
 print("\n> Сценарий 6: Демонстрация валидации через setter")
 try:
-    print("-Попытка установить GPA = 5.5 (выходит за пределы 0-5)...")
+    print("-Попытка установить GPA = 5.5 (выходит за пределы 0-5)")
     student2.gpa = 5.5
 except ValueError as e:
-    print(f"Ошибка валидации: {e}")
+    print(f"Ошибка валидации: {e}\n")
 
 try:
-    print("-Попытка установить возраст = 70 (выходит за пределы 16-60)...")
+    print("-Попытка установить возраст = 70 (выходит за пределы 16-60)")
     student2.age = 70
 except ValueError as e:
-    print(f"Ошибка валидации: {e}")
+    print(f"Ошибка валидации: {e}\n")
 
 try:
-    print("-Попытка установить курс = 7 (выходит за пределы 1-6)...")
+    print("-Попытка установить курс = 7 (выходит за пределы 1-6)")
     student2.curse = 7
 except ValueError as e:
-    print(f"Ошибка валидации: {e}")
+    print(f"Ошибка валидации: {e}\n")
